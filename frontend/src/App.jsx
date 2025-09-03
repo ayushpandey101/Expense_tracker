d7c7c9fb7e1adce67d3d7bad0f60c7550072e19c
Ayush Pandey <sjas.ayushpandey101@gmail.com>
2025-09-03 17:56:27 +0530

    style: update route paths in App component for consistency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,12 +21,12 @@ const App = () => {
       <div>
         <Router>
           <Routes>
-            <Route path="" element={<Root />} />
-            <Route path="login" exact element={<Login />} />
-            <Route path="signup" exact element={<Signup />} />
-            <Route path="dashboard" exact element={<Home />} />
-            <Route path="income" exact element={<Income />} />
-            <Route path="expense" exact element={<Expense />} />
+            <Route path="/" element={<Root />} />
+            <Route path="/login" exact element={<Login />} />
+            <Route path="/signup" exact element={<Signup />} />
+            <Route path="/dashboard" exact element={<Home />} />
+            <Route path="/income" exact element={<Income />} />
+            <Route path="/expense" exact element={<Expense />} />
           </Routes>
         </Router>
       </div>
